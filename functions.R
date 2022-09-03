13dcb6ef8e0b297fca4c455e04d76923810b9eb6
Alice Jonason <alicejonason@gmail.com>
2022-09-03 12:04:39 +0200

    added toeplitz matrix

--- functions.R
+++ functions.R
@@ -1,3 +1,17 @@
+toeplitz_matrix <- function(x) { # Vector of values used to construct the matrix
+  if (length(x) %% 2 == 0) stop("x not of odd length")
+  n <- (length(x) + 1) / 2
+  toeplitz_mat <- matrix(nrow = n, ncol = n)
+  x <- rev(x)
+  x <- c(x[-n:-length(x)], rev(x[n:length(x)]))
+  for (i in 1:nrow(toeplitz_mat)) {
+    for (j in 1:ncol(toeplitz_mat)) {
+      toeplitz_mat[i, j] <- x[n - (i - j)]
+    }
+  }
+    return(toeplitz_mat)
+}
+
 # Function to calculate the derivative of a function x
 derivative <- function(f, # function to take derivative of
                        var_name) { # variable (char) to take derivative w.r.t.
